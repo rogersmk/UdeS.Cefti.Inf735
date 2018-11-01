d74db401c61ab20a3bf80611b28460897f3ad6e9
escenr70 <escenr70@LGI18Q3044>
2018-11-01 13:42:28 -0400

    update

--- Tp01/Ex01/drop_create_table-dbo.FactDrumSales.sql
+++ Tp01/Ex01/drop_create_table-dbo.FactDrumSales.sql
@@ -1,6 +1,14 @@
 USE [UdeS.Cefti.Inf735.Lab01.Ex1.DW]
 GO
 
+EXEC sys.sp_dropextendedproperty @name=N'MS_Description' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
+	@level2type=N'CONSTRAINT',@level2name=N'FK_FactDrumSales_DimRegion'
+GO
+
+EXEC sys.sp_dropextendedproperty @name=N'MS_Description' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
+	@level2type=N'CONSTRAINT',@level2name=N'FK_FactDrumSales_DimAccessory'
+GO
+
 EXEC sys.sp_dropextendedproperty @name=N'MS_Description' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales'
 GO
 
@@ -92,6 +100,30 @@ EXEC sys.sp_dropextendedproperty @name=N'MS_Description' , @level0type=N'SCHEMA'
 	@level2type=N'COLUMN',@level2name=N'Id'
 GO
 
+ALTER TABLE [dbo].[FactDrumSales] DROP CONSTRAINT [FK_FactDrumSales_DimRegion]
+GO
+
+ALTER TABLE [dbo].[FactDrumSales] DROP CONSTRAINT [FK_FactDrumSales_DimFormat]
+GO
+
+ALTER TABLE [dbo].[FactDrumSales] DROP CONSTRAINT [FK_FactDrumSales_DimDrum]
+GO
+
+ALTER TABLE [dbo].[FactDrumSales] DROP CONSTRAINT [FK_FactDrumSales_DimDistributor]
+GO
+
+ALTER TABLE [dbo].[FactDrumSales] DROP CONSTRAINT [FK_FactDrumSales_DimDesign]
+GO
+
+ALTER TABLE [dbo].[FactDrumSales] DROP CONSTRAINT [FK_FactDrumSales_DimDate]
+GO
+
+ALTER TABLE [dbo].[FactDrumSales] DROP CONSTRAINT [FK_FactDrumSales_DimClient]
+GO
+
+ALTER TABLE [dbo].[FactDrumSales] DROP CONSTRAINT [FK_FactDrumSales_DimAccessory]
+GO
+
 ALTER TABLE [dbo].[FactDrumSales] DROP CONSTRAINT [DF_FactDrumSales_TransportFeesUSD]
 GO
 
@@ -128,11 +160,11 @@ GO
 ALTER TABLE [dbo].[FactDrumSales] DROP CONSTRAINT [DF_FactDrumSales_Quantity]
 GO
 
-/****** Object:  Table [dbo].[FactDrumSales]    Script Date: 10/31/2018 8:47:56 PM ******/
+/****** Object:  Table [dbo].[FactDrumSales]    Script Date: 11/1/2018 1:12:47 PM ******/
 DROP TABLE [dbo].[FactDrumSales]
 GO
 
-/****** Object:  Table [dbo].[FactDrumSales]    Script Date: 10/31/2018 8:47:56 PM ******/
+/****** Object:  Table [dbo].[FactDrumSales]    Script Date: 11/1/2018 1:12:47 PM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -217,94 +249,199 @@ ALTER TABLE [dbo].[FactDrumSales]
 ADD  CONSTRAINT [DF_FactDrumSales_TransportFeesUSD]				DEFAULT ((1)) FOR [TransportFeesUSD]
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales Id' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
+ALTER TABLE [dbo].[FactDrumSales]  WITH CHECK ADD
+CONSTRAINT [FK_FactDrumSales_DimAccessory]
+FOREIGN KEY([DistributorKey])		REFERENCES [dbo].[DimAccessory] ([Id])
+GO
+
+ALTER TABLE [dbo].[FactDrumSales] CHECK
+CONSTRAINT [FK_FactDrumSales_DimAccessory]
+GO
+
+ALTER TABLE [dbo].[FactDrumSales]  WITH CHECK ADD
+CONSTRAINT [FK_FactDrumSales_DimClient]
+FOREIGN KEY([ClientKey])			REFERENCES [dbo].[DimClient] ([Id])
+GO
+
+ALTER TABLE [dbo].[FactDrumSales] CHECK
+CONSTRAINT [FK_FactDrumSales_DimClient]
+GO
+
+ALTER TABLE [dbo].[FactDrumSales]  WITH CHECK ADD
+CONSTRAINT [FK_FactDrumSales_DimDate]
+FOREIGN KEY([DateKey])				REFERENCES [dbo].[DimDate] ([Id])
+GO
+
+ALTER TABLE [dbo].[FactDrumSales] CHECK
+CONSTRAINT [FK_FactDrumSales_DimDate]
+GO
+
+ALTER TABLE [dbo].[FactDrumSales]  WITH CHECK ADD
+CONSTRAINT [FK_FactDrumSales_DimDesign]
+FOREIGN KEY([DesignKey])			REFERENCES [dbo].[DimDesign] ([Id])
+GO
+
+ALTER TABLE [dbo].[FactDrumSales] CHECK
+CONSTRAINT [FK_FactDrumSales_DimDesign]
+GO
+
+ALTER TABLE [dbo].[FactDrumSales]  WITH CHECK ADD
+CONSTRAINT [FK_FactDrumSales_DimDistributor]
+FOREIGN KEY([DistributorKey])		REFERENCES [dbo].[DimDistributor] ([Id])
+GO
+
+ALTER TABLE [dbo].[FactDrumSales] CHECK
+CONSTRAINT [FK_FactDrumSales_DimDistributor]
+GO
+
+ALTER TABLE [dbo].[FactDrumSales]  WITH CHECK ADD
+CONSTRAINT [FK_FactDrumSales_DimDrum]
+FOREIGN KEY([DrumKey])				REFERENCES [dbo].[DimDrum] ([Id])
+GO
+
+ALTER TABLE [dbo].[FactDrumSales] CHECK
+CONSTRAINT [FK_FactDrumSales_DimDrum]
+GO
+
+ALTER TABLE [dbo].[FactDrumSales]  WITH CHECK ADD
+CONSTRAINT [FK_FactDrumSales_DimFormat]
+FOREIGN KEY([FormatKey])			REFERENCES [dbo].[DimFormat] ([Id])
+GO
+
+ALTER TABLE [dbo].[FactDrumSales] CHECK
+CONSTRAINT [FK_FactDrumSales_DimFormat]
+GO
+
+ALTER TABLE [dbo].[FactDrumSales]  WITH CHECK ADD
+CONSTRAINT [FK_FactDrumSales_DimRegion]
+FOREIGN KEY([RegionKey])			REFERENCES [dbo].[DimRegion] ([Id])
+GO
+
+ALTER TABLE [dbo].[FactDrumSales] CHECK
+CONSTRAINT [FK_FactDrumSales_DimRegion]
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales Id' ,
+	@level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
 	@level2type=N'COLUMN',@level2name=N'Id'
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales Distributor Key' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales Distributor Key' ,
+	@level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
 	@level2type=N'COLUMN',@level2name=N'DistributorKey'
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales Client Key' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales Client Key' ,
+	@level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
 	@level2type=N'COLUMN',@level2name=N'ClientKey'
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales Drum Key' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales Drum Key' ,
+	@level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
 	@level2type=N'COLUMN',@level2name=N'DrumKey'
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales Format Key' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales Format Key' ,
+	@level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
 	@level2type=N'COLUMN',@level2name=N'FormatKey'
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales Accessory Key' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales Accessory Key' ,
+	@level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
 	@level2type=N'COLUMN',@level2name=N'AccessoryKey'
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales Design Key' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales Design Key' ,
+	@level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
 	@level2type=N'COLUMN',@level2name=N'DesignKey'
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales Date Key' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales Date Key' ,
+	@level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
 	@level2type=N'COLUMN',@level2name=N'DateKey'
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales Region Key' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales Region Key' ,
+	@level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
 	@level2type=N'COLUMN',@level2name=N'RegionKey'
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales Quantity' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales Quantity' ,
+	@level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
 	@level2type=N'COLUMN',@level2name=N'Quantity'
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales-Total Sales Amount in CND' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales-Total Sales Amount in CND' ,
+	@level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
 	@level2type=N'COLUMN',@level2name=N'TotalSalesAmountCND'
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales-Accessory Sales Amount CND' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales-Accessory Sales Amount CND' ,
+	@level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
 	@level2type=N'COLUMN',@level2name=N'AccessorySalesAmountCND'
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales-Sales Commission CND' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales-Sales Commission CND' ,
+	@level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
 	@level2type=N'COLUMN',@level2name=N'SalesCommissionCND'
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales-Transport Commission CND' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales-Transport Commission CND' ,
+	@level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
 	@level2type=N'COLUMN',@level2name=N'TransportCommissionCND'
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales-Transport Fees CND' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales-Transport Fees CND' ,
+	@level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
 	@level2type=N'COLUMN',@level2name=N'TransportFeesCND'
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales-Transport Fees Canada Post CND' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales-Transport Fees Canada Post CND' ,
+	@level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
 	@level2type=N'COLUMN',@level2name=N'TransportFeesCanadaPostCND'
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales-Change Rate' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales-Change Rate' ,
+	@level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
 	@level2type=N'COLUMN',@level2name=N'ChangeRate'
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales-Total Sales Amount USD' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales-Total Sales Amount USD' ,
+	@level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
 	@level2type=N'COLUMN',@level2name=N'TotalSalesAmountUSD'
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales-Accessory Sales Amount USD' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales-Accessory Sales Amount USD' ,
+	@level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
 	@level2type=N'COLUMN',@level2name=N'AccessorySalesAmountUSD'
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales-Sales Commission USD' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales-Sales Commission USD' ,
+	@level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
 	@level2type=N'COLUMN',@level2name=N'SalesCommissionUSD'
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales-Transport Commission USD' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales-Transport Commission USD' ,
+	@level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
 	@level2type=N'COLUMN',@level2name=N'TransportCommissionUSD'
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales-Transport Fees USD' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales-Transport Fees USD' ,
+	@level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
 	@level2type=N'COLUMN',@level2name=N'TransportFeesUSD'
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales'
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fact Drum Sales' ,
+	@level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'FK to FactDrumSales from DimAccessory' ,
+	@level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
+	@level2type=N'CONSTRAINT',@level2name=N'FK_FactDrumSales_DimAccessory'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'FK to FactDrumSales from DimRegion' ,
+	@level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FactDrumSales',
+	level2type=N'CONSTRAINT',@level2name=N'FK_FactDrumSales_DimRegion'
 GO
 
