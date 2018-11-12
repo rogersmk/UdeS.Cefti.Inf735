6506255a505b542f601c752d5c2b680a8a0babde
escenr70 <escenr70@LGI18Q3044>
2018-11-12 18:17:47 -0500

    base data

---
+++ Lab02/Inf735-Lab02_DimPlageAge.ms.sql
@@ -0,0 +1,6 @@
+USE [UdeS.Cefti.Inf735.Lab01.Lab02.DW]
+GO
+
+INSERT INTO	[dbo].[DimPlageAge]	([PlageAge], [PlageAgeTri], [AgeMedian])
+	VALUES	('', '', 0)
+GO
---
+++ Lab02/Inf735-Lab02_DimProvince.ms.sql
@@ -0,0 +1,8 @@
+USE [UdeS.Cefti.Inf735.Lab01.Lab02.DW]
+GO
+
+INSERT INTO	[dbo].[DimProvince]	([Name])
+	VALUES	('')
+GO
+
+
